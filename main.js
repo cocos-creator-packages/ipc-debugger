97fe141e48776bc3a1ed6b4bf60a7fdc19bc2b10
Johnny Wu <KarasAya@gmail.com>
2016-02-14 17:51:55 +0800

    update main process message registry

--- main.js
+++ main.js
@@ -17,54 +17,56 @@ module.exports = {
     }
   },
 
-  'ipc-debugger:open' () {
-    Editor.Panel.open('ipc-debugger.panel');
-  },
+  messages: {
+    open () {
+      Editor.Panel.open('ipc-debugger.panel');
+    },
 
-  'ipc-debugger:query' ( event, reply ) {
-    let windows = Editor.Window.windows;
-    let infoList = [];
+    query ( event, reply ) {
+      let windows = Editor.Window.windows;
+      let infoList = [];
 
-    for ( let p in ipcMain._events ) {
-      let listeners = ipcMain._events[p];
-      let count = Array.isArray(listeners) ? listeners.length : 1;
-      infoList.push({
-        name: p,
-        level: 'core',
-        count: count,
-        inspect: _inspects[p] !== undefined,
+      for ( let p in ipcMain._events ) {
+        let listeners = ipcMain._events[p];
+        let count = Array.isArray(listeners) ? listeners.length : 1;
+        infoList.push({
+          name: p,
+          level: 'core',
+          count: count,
+          inspect: _inspects[p] !== undefined,
+        });
+      }
+
+      Async.each( windows, ( win, done ) => {
+        win.sendRequestToPage( 'editor:query-ipc-events', infos => {
+          if ( infos ) {
+            infoList = infoList.concat(infos);
+          }
+
+          done();
+        });
+      }, () => {
+        reply(infoList);
       });
-    }
+    },
 
-    Async.each( windows, ( win, done ) => {
-      win.sendRequestToPage( 'editor:query-ipc-events', infos => {
-        if ( infos ) {
-          infoList = infoList.concat(infos);
-        }
+    inspect ( event, name ) {
+      function fn () {
+        let args = [].slice.call( arguments, 0 );
+        args.unshift( 'ipc-debugger[core][' + name + ']' );
+        Editor.success.apply( Editor, args );
+      }
 
-        done();
-      });
-    }, () => {
-      reply(infoList);
-    });
-  },
+      _inspects[name] = fn;
+      ipcMain.on( name, fn );
+    },
 
-  'ipc-debugger:inspect' ( event, name ) {
-    function fn () {
-      let args = [].slice.call( arguments, 0 );
-      args.unshift( 'ipc-debugger[core][' + name + ']' );
-      Editor.success.apply( Editor, args );
-    }
-
-    _inspects[name] = fn;
-    ipcMain.on( name, fn );
-  },
-
-  'ipc-debugger:uninspect' ( event, name ) {
-    let fn = _inspects[name];
-    if ( fn ) {
-      ipcMain.removeListener( name, fn );
-      delete _inspects[name];
-    }
-  },
+    uninspect ( event, name ) {
+      let fn = _inspects[name];
+      if ( fn ) {
+        ipcMain.removeListener( name, fn );
+        delete _inspects[name];
+      }
+    },
+  }
 };
